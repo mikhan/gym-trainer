2f33c3a5ac4491dba939e67af66959ccc8045218
Jorge Rojas <mikhan_@hotmail.com>
2024-09-27 11:33:24 -0700

    temp

--- src/routes/(app)/trainer/TrainerNavigator.svelte
+++ src/routes/(app)/trainer/TrainerNavigator.svelte
@@ -9,6 +9,7 @@
 
   const { data, onselect = () => {} }: Props = $props()
   const groupedData = $derived(Object.groupBy(data.currentRoutine.series, (serie) => serie.group))
+  let currentSerieId = $state(data.currentSerie.id)
 </script>
 
 <section class="flex flex-wrap items-center gap-2">
@@ -16,25 +17,29 @@
     <div class="w-full typescale-label font-bold uppercase">{group}</div>
     {#each series as serie}
       {@const percent = `${((data.progress[serie.id] ?? 0) * 100).toFixed(0)}%`}
-      <button
+      <label
         class={clsx(
-          'group stack relative min-w-min rounded-button text-left',
-          data.currentSerie.id === serie.id && 'ring-4 ring-secondary-line',
-        )}
-        type="button"
-        onclick={() => onselect(data.currentRoutine.series.indexOf(serie))}>
+          'group stack relative min-w-min rounded-button shadow text-left focusable-within-ring border-4 contain-paint',
+          currentSerieId === serie.id ? 'border-primary' : 'border-default-line',
+        )}>
         <div
-          class="line-clamp-1 px-2 py-1 color-neutral bg-default group-hover:bg-default-hover group-active:bg-default-active transition-all rounded-button shadow">
+          class="line-clamp-1 px-2 py-1 color-neutral bg-default group-hover:bg-default-hover group-active:bg-default-active transition-all">
           {serie.name}
         </div>
         <div
-          class="line-clamp-1 px-2 py-1 color-secondary bg-default group-hover:bg-default-hover group-active:bg-default-active transition-all rounded-button shadow"
+          class="line-clamp-1 px-2 py-1 color-secondary-darker bg-default group-hover:bg-default-hover group-active:bg-default-active transition-all"
           style="clip-path: rect(auto var(--clip-width) auto auto)"
           style:--clip-width={percent}>
           {serie.name}
         </div>
-        <div class="absolute -inset-2"></div>
-      </button>
+        <input
+          class="sr-only"
+          type="radio"
+          name="currentSerieId"
+          bind:group={currentSerieId}
+          value={serie.id}
+          onchange={() => onselect(data.currentRoutine.series.indexOf(serie))} />
+      </label>
     {/each}
   {/each}
 </section>
